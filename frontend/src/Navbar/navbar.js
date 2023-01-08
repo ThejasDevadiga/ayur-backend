81e6c138985a23bfcc76032c09e6c14217185be1
ThejasDevadiga <4al20ai047@gmail.com>
2023-01-08 19:01:17 +0530

    08-01-23

--- frontend/src/Navbar/navbar.js
+++ frontend/src/Navbar/navbar.js
@@ -1,14 +1,3 @@
-// const navbar = document.querySelector('.navbar');
-// let top = navbar.offsetTop;
-// function stickynavbar() {
-//   if (window.scrollY >= top) {    
-//     navbar.classList.add('sticky');
-//   } else {
-//     navbar.classList.remove('sticky');    
-//   }
-// }
-// window.addEventListener('scroll', stickynavbar);
-
 const fullScreenBtn = document.querySelector('.fullScreen')
 const fullBody = document.querySelector('body')
 function fullScreenMode (){
